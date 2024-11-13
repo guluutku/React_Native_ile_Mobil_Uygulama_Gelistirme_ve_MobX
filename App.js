64c564e008678ddaf68a064f849a13980fc780ba
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-11-13 20:54:13 +0300

    Learned flexDirection

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-trailing-spaces */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -16,7 +17,11 @@ function App() {
   return (
     <View style={styles.container}>
       <View style={styles.slideOne} />
-      <View style={styles.slideTwo} />
+      <View style={styles.slideTwo} >
+        <View style={[styles.box, styles.box1]} />
+        <View style={[styles.box, styles.box2]} />
+        <View style={[styles.box, styles.box3]} />
+      </View>
     </View>
   );
 }
@@ -28,11 +33,26 @@ const styles = StyleSheet.create({
   },
   slideOne: {
     backgroundColor: '#607D8B',
-    flex: 1, // 3:6
+    flex: 1,
   },
   slideTwo: {
     backgroundColor: '#FF5722',
-    flex: 1, // 3:6
+    flex: 2,
+    flexDirection: 'row',
+  },
+
+  box: {
+    width: 100,
+    height: 100,
+  },
+  box1: {
+    backgroundColor: '#795548',
+  },
+  box2: {
+    backgroundColor: '#9E9E9E',
+  },
+  box3: {
+    backgroundColor: '#3F51B5',
   },
 });
 

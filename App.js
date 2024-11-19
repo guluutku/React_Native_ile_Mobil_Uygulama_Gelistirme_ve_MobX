fa03edacb2c64afc6b838de71b1d49123a93a712
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-11-19 21:40:59 +0300

    Creating login area

--- App.js
+++ App.js
@@ -7,21 +7,73 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
+  View,
 } from 'react-native';
 
 function App() {
 
   return (
-    <SafeAreaView>
-      <Text>New App</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.headBackground} />
+
+      <View>
+        <Text style={styles.logo}>UDAC</Text>
+        <Text style={styles.logoDescription}>Property & Survey</Text>
+      </View>
+
+      <ScrollView>
+        <View style={styles.loginArea}>
+
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    paddingVertical: 80,
+  },
+  headBackground: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: 250,
+    width: '100%',
+    backgroundColor: '#1572DE',
+  },
+  logo: {
+    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#F2f2f2',
+  },
+  logoDescription: {
+    textAlign: 'center',
+    color: '#f2f2f2',
+  },
+  loginArea: {
+    marginHorizontal: 40,
+    marginVertical: 40,
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 5,
+
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+
+    elevation: 4,
+  },
 });
 
 export default App;

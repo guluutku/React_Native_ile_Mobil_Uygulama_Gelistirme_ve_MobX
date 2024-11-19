c6802e6fc26819a45c11fdbfba02f1606d812caf
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-11-19 18:26:53 +0300

    Events

--- App.js
+++ App.js
@@ -9,17 +9,28 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
+  Button,
 } from 'react-native';
 
 import Card from './src/components/Card';
 
 class App extends Component {
 
+  onPressDetail = () => {
+    // eslint-disable-next-line no-alert
+    alert('Detail Alert');
+  };
+
   render() {
     return (
       <View style={[styles.container]} >
         <Card text="Merhaba" backgroundColor="blue" />
-        <Card text="React" backgroundColor="red" />
+        <Card text="React Native" backgroundColor="red" />
+        <Button
+          title="Details"
+          color="#000"
+          onPress={this.onPressDetail}
+        />
       </View>
     );
   }
@@ -30,7 +41,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#FFEB3B',
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignContent: 'center',
   },

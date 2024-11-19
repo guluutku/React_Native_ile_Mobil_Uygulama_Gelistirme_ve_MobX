c9bd91d557ff7a904c9a8d2ab06aea8fdefdde10
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-11-19 20:00:47 +0300

    Update App.js

--- App.js
+++ App.js
@@ -9,30 +9,14 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text,
-  ScrollView,
-  Dimensions,
 } from 'react-native';
-
-const { width } = Dimensions.get('window');
 
 class App extends Component {
 
   render() {
     return (
       <View style={[styles.container]} >
-        <ScrollView horizontal={true}>
-          <Text style={styles.title}>1</Text>
-          <Text style={styles.title}>2</Text>
-          <Text style={styles.title}>3</Text>
-          <Text style={styles.title}>4</Text>
-          <Text style={styles.title}>5</Text>
-          <Text style={styles.title}>6</Text>
-          <Text style={styles.title}>7</Text>
-          <Text style={styles.title}>8</Text>
-          <Text style={styles.title}>9</Text>
-          <Text style={styles.title}>10</Text>
-        </ScrollView>
+
       </View >
     );
   }
@@ -41,15 +25,7 @@ class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFEB3B',
     flex: 1,
-  },
-  title: {
-    backgroundColor: 'orange',
-    marginVertical: 30,
-    textAlign: 'center',
-    paddingVertical: 60,
-    width,
   },
 });
 

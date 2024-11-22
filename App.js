ee67bcfad2f72963d0788a6a0ec18af15e0da766
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-11-22 09:41:47 +0300

    Login Area Added

--- App.js
+++ App.js
@@ -1,79 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+
+import Login from './src/pages/Login';
 
 function App() {
-
   return (
-    <View style={styles.container}>
-      <View style={styles.headBackground} />
-
-      <View>
-        <Text style={styles.logo}>UDAC</Text>
-        <Text style={styles.logoDescription}>Property & Survey</Text>
-      </View>
-
-      <ScrollView>
-        <View style={styles.loginArea}>
-
-        </View>
-      </ScrollView>
-    </View>
+    <Login />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    paddingVertical: 80,
-  },
-  headBackground: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    height: 250,
-    width: '100%',
-    backgroundColor: '#1572DE',
-  },
-  logo: {
-    textAlign: 'center',
-    fontSize: 40,
-    fontWeight: 'bold',
-    color: '#F2f2f2',
-  },
-  logoDescription: {
-    textAlign: 'center',
-    color: '#f2f2f2',
-  },
-  loginArea: {
-    marginHorizontal: 40,
-    marginVertical: 40,
-    backgroundColor: '#fff',
-    padding: 20,
-    borderRadius: 5,
-
-    shadowColor: 'black',
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-
-    elevation: 4,
-  },
-});
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+function Login() {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.headBackground} />
+
+            <View>
+                <Text style={styles.logo}>UDAC</Text>
+                <Text style={styles.logoDescription}>Property & Survey</Text>
+            </View>
+
+            <ScrollView>
+                <View style={styles.loginArea}>
+                    <Text style={styles.loginAreaTitle}>Property Tax Server</Text>
+                    <Text style={styles.loginAreaDescription}>Unique Door No Easily Fill Your Entire Property Tax Using App</Text>
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+        paddingVertical: 80,
+    },
+    headBackground: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: 250,
+        width: '100%',
+        backgroundColor: '#1572DE',
+    },
+    logo: {
+        textAlign: 'center',
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#F2f2f2',
+    },
+    logoDescription: {
+        textAlign: 'center',
+        color: '#f2f2f2',
+    },
+
+    loginArea: {
+        marginHorizontal: 40,
+        marginVertical: 40,
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 5,
+
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+
+        elevation: 4,
+    },
+    loginAreaTitle: {
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    loginAreaDescription: {
+        fontSize: 12,
+        color: '#7E868F',
+        marginVertical: 10,
+        textAlign: 'center',
+    },
+});
+
+export default Login;

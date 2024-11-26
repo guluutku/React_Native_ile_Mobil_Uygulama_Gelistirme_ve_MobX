9890ce5dae9fef5cff48a2e08c597a00b923ee35
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-11-26 15:10:24 +0300

    Navigation native-stack added

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
 } from 'react-native';
+import { createAppContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+
+  return (
+    <SafeAreaView>
+      <Text>New Home!!!!</Text>
+    </SafeAreaView>
+  );
+
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 function App() {
 
   return (
-    <SafeAreaView>
-      <Text>New App</Text>
-    </SafeAreaView>
+    <AppContainer />
   );
+
 }
 
 const styles = StyleSheet.create({

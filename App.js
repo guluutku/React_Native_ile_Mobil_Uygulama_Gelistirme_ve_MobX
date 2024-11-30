43ffef1dda344760bb2e1f5baebb1f70185c5505
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-11-30 15:08:44 +0300

    Put router in a different file

--- App.js
+++ App.js
@@ -1,52 +1,6 @@
 import React from 'react';
-import { createStaticNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {
-  Button,
-} from 'react-native';
 
-import HomeScreen from './src/components/screens/HomeScreen';
-import DetailScreen from './src/components/screens/DetailScreen';
-
-const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Home',
-
-  screenOptions: {
-    headerBackTitle: 'Geri',
-    headerTintColor: '#333',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-  },
-
-  screens: {
-    Home: {
-      screen: HomeScreen,
-      options: {
-        headerRight: () => (
-          <Button
-            // eslint-disable-next-line no-alert
-            onPress={() => alert('This is a button!')}
-            title="Right"
-          />
-        ),
-      },
-    },
-
-    Detail: {
-      screen: DetailScreen,
-      initialParams: { title: 'Default Title' },
-      options: ({ route }) => ({
-        title: route.params.title,
-        headerStyle: {
-          backgroundColor: 'yellow',
-        },
-      }),
-    },
-  },
-});
-
-const Navigation = createStaticNavigation(RootStack);
+import Navigation from './src/Router';
 
 export default function App() {
 

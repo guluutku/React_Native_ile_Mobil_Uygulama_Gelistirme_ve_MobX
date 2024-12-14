281ba2ad1a6239e9ac473b5704f72300297f3342
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-12-14 15:31:18 +0300

    Timing

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Text,
-} from 'react-native';
+import Timing from './components/Timing';
 
 function App() {
 
   return (
-    <View style={styles.container}>
-      <Text>New App</Text>
-    </View>
+    <Timing />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
